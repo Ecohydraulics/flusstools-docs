165c1608edf2b213c0a78292b87cf7023f140a55
sschwindt <schwindt.water@gmail.com>
2020-11-17 10:16:41 +0100

    modified imports

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         "click",
         "pydata-sphinx-theme~=0.4.1",
         "beautifulsoup4",
-        "flusstools",
+        "flusstools @ git+ssh://git@github.com/Ecohydraulics/flusstools-pckg.git@main#flusstools",
         'importlib-resources~=3.0.0; python_version < "3.7"',
     ],
     dependency_links=[
