c1ba8e10d9cc37de08ea45c7a538ea95d9fafb1b
sschwindt <schwindt.water@gmail.com>
2020-11-17 14:25:15 +0100

    fixed dependencies

--- site-packages/alphashape/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Top-level package for Alpha Shape Toolbox."""
-
-__author__ = """Kenneth E. Bellock"""
-__email__ = 'ken@bellock.net'
-
-from .alphashape import alphashape
-from .optimizealpha import optimizealpha
-from ._version import __version__  # noqa: F401
-__all__ = ['alphashape', 'optimizealpha']
