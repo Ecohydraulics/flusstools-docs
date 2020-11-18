5334b8c9f29fa325a322fa61047db3e8a8c49259
sschwindt <schwindt.water@gmail.com>
2020-11-18 07:39:08 +0100

    formatting steps

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    # 'sphinx.ext.napoleon',
+    'sphinx.ext.napoleon',
     'sphinx.ext.githubpages',
     'sphinx.ext.todo',
     'sphinx_thebe',
