dbaa622110d1bac2f9fed8818fc0034c5ef4180a
sschwindt <schwindt.water@gmail.com>
2021-02-09 09:54:47 +0100

    updated bibs

--- docs/conf.py
+++ docs/conf.py
@@ -12,18 +12,18 @@ import datetime
 
 def is_development_build():
     # PR builds have an interger version
-    re_version = re.compile(r'^[\d]+$')
-    if 'READTHEDOCS' in os.environ:
-        version = os.environ.get('READTHEDOCS_VERSION', '')
+    re_version = re.compile(r"^[\d]+$")
+    if "READTHEDOCS" in os.environ:
+        version = os.environ.get("READTHEDOCS_VERSION", "")
         if re_version.match(version):
             return True
         return False
     return True
 
 
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.append(os.path.abspath('..') + '/examples/fuzzycorr-showcase/')
-sys.path.append(os.path.abspath('..') + '/examples/geotools-showcase/')
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.append(os.path.abspath("..") + "/examples/fuzzycorr-showcase/")
+sys.path.append(os.path.abspath("..") + "/examples/geotools-showcase/")
 
 # the following modules will be mocked (i.e. bogus imports - required for C-dependent packages)
 autodoc_mock_imports = [
@@ -50,47 +50,47 @@ autodoc_mock_imports = [
 import sphinx_rtd_theme
 from sphinx.locale import _
 
-project = u'FlussTools'
-slug = re.sub(r'\W+', '-', project.lower())
-version = '0.1'
-release = 'latest'
-author = u'Sebastian Schwindt, Beatriz Negreiros\nKilian Mouris, Kenneth Larrieu'
+project = u"FlussTools"
+slug = re.sub(r"\W+", "-", project.lower())
+version = "0.1"
+release = "latest"
+author = u"Sebastian Schwindt, Beatriz Negreiros\nKilian Mouris, Kenneth Larrieu"
 copyright = author
-language = 'en'
+language = "en"
 
 extensions = [
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.intersphinx",
     "sphinx_copybutton",
     "sphinx_togglebutton",
-    "sphinxcontrib.bibtex",
-    'sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.githubpages',
-    'sphinx.ext.todo',
-    'sphinx_thebe',
-    # 'sphinxcontrib.googleanalytics',
-    'IPython.sphinxext.ipython_console_highlighting',
-    'IPython.sphinxext.ipython_directive',
-    'myst_nb',
-    'jupyter_sphinx',
+    # "sphinxcontrib.bibtex",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.todo",
+    "sphinx_thebe",
+    # "sphinxcontrib.googleanalytics",
+    "IPython.sphinxext.ipython_console_highlighting",
+    "IPython.sphinxext.ipython_directive",
+    "myst_nb",
+    "jupyter_sphinx",
 ]
 
-templates_path = ['_templates']
-source_suffix = '.rst'
+templates_path = ["_templates"]
+source_suffix = ".rst"
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-locale_dirs = ['locale/', 'docs/']
+locale_dirs = ["locale/", "docs/"]
 gettext_compact = False
 
-master_doc = 'index'
-suppress_warnings = ['image.nonlocal_uri']
-pygments_style = 'sphinx'
+master_doc = "index"
+suppress_warnings = ["image.nonlocal_uri"]
+pygments_style = "sphinx"
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3.8", None),
-    'rtd': ('https://docs.readthedocs.io/en/latest/', None),
-    'sphinx': ('http://www.sphinx-doc.org/en/stable/', None),
+    "rtd": ("https://docs.readthedocs.io/en/latest/", None),
+    "sphinx": ("http://www.sphinx-doc.org/en/stable/", None),
 }
 
 nitpick_ignore = [
@@ -105,46 +105,46 @@ myst_deflist_enable = True
 myst_url_schemes = ("http", "https", "mailto")
 panels_add_bootstrap_css = False
 
-html_theme = 'sphinx_book_theme'
+html_theme = "sphinx_book_theme"
 html_theme_options = {
 
     "theme_dev_mode": True,
-    'launch_buttons': {
-        'binderhub_url': 'https://mybinder.org',
-        'thebe': True,
-        'notebook_interface': 'jupyterlab',
-        'collapse_navigation': False
+    "launch_buttons": {
+        "binderhub_url": "https://mybinder.org",
+        "thebe": True,
+        "notebook_interface": "jupyterlab",
+        "collapse_navigation": False
     },
-    'repository_url': 'https://github.com/ecohydraulics/flusstools-pckg/',
-    'repository_branch': 'main',
-    'use_edit_page_button': False,
-    'use_repository_button': True,
+    "repository_url": "https://github.com/ecohydraulics/flusstools-pckg/",
+    "repository_branch": "main",
+    "use_edit_page_button": False,
+    "use_repository_button": True,
 }
 
 html_context = {
-    'date': datetime.date.today().strftime('%Y-%m-%d'),
-    'display_github': True,
-    'github_user': 'Ecohydraulics',
-    'github_repo': 'flusstools-docs',
-    'github_version': 'main/',
-    'conf_py_path': '/docs/'
+    "date": datetime.date.today().strftime("%Y-%m-%d"),
+    "display_github": True,
+    "github_user": "Ecohydraulics",
+    "github_repo": "flusstools-docs",
+    "github_version": "main/",
+    "conf_py_path": "/docs/"
 }
 
-if not ('READTHEDOCS' in os.environ):
-    html_static_path = ['_static/']
-    html_js_files = ['debug.js']
+if not ("READTHEDOCS" in os.environ):
+    html_static_path = ["_static/"]
+    html_js_files = ["debug.js"]
 
     # Add fake versions for local QA of the menu
-    html_context['test_versions'] = list(map(
+    html_context["test_versions"] = list(map(
         lambda x: str(x / 10),
         range(1, 100)
     ))
 
 html_last_updated_fmt = ""
-html_logo = os.path.abspath('..') + '/docs/img/icon.svg'
+html_logo = os.path.abspath("..") + "/docs/img/icon.svg"
 html_show_sourcelink = True
 html_title = "FlussTools " + version
-htmlhelp_basename = 'FlussTools'
+htmlhelp_basename = "FlussTools"
 html_copy_source = True
 html_sourcelink_suffix = ""
 
@@ -155,7 +155,7 @@ thebe_config = {
 }
 
 latex_documents = [
-  (master_doc, '{0}.tex'.format(slug), project, author, 'manual'),
+  (master_doc, "{0}.tex".format(slug), project, author, "manual"),
 ]
 
 man_pages = [
@@ -167,7 +167,7 @@ execution_allow_errors = True
 jupyter_execute_notebooks = "auto"
 
 texinfo_documents = [
-  (master_doc, slug, project, author, slug, project, 'Miscellaneous'),
+  (master_doc, slug, project, author, slug, project, "Miscellaneous"),
 ]
 
 
@@ -177,23 +177,23 @@ def setup(app):
     from sphinx.util.docfields import Field
 
     app.add_object_type(
-        'confval',
-        'confval',
-        objname='configuration value',
-        indextemplate='pair: %s; configuration value',
+        "confval",
+        "confval",
+        objname="configuration value",
+        indextemplate="pair: %s; configuration value",
         doc_field_types=[
             PyField(
-                'type',
-                label=_('Type'),
+                "type",
+                label=_("Type"),
                 has_arg=False,
-                names=('type',),
-                bodyrolename='class'
+                names=("type",),
+                bodyrolename="class"
             ),
             Field(
-                'default',
-                label=_('Default'),
+                "default",
+                label=_("Default"),
                 has_arg=False,
-                names=('default',),
+                names=("default",),
             ),
         ]
     )
